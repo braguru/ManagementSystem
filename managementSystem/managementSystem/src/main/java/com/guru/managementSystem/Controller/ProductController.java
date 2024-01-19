0aa014b8528b8d1ba6ae4aa897a42666dfb2c70c
Prince Ofori Ankamah <prince.ankamah@amalitech.com>
2024-01-19 14:09:08 +0000

    testing product endpoint

--- managementSystem/managementSystem/src/main/java/com/guru/managementSystem/Controller/ProductController.java
+++ managementSystem/managementSystem/src/main/java/com/guru/managementSystem/Controller/ProductController.java
@@ -3,13 +3,7 @@ package com.guru.managementSystem.Controller;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.DeleteMapping;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.PutMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import com.guru.managementSystem.Entity.Product;
 import com.guru.managementSystem.Service.ProductService;
@@ -40,8 +34,8 @@ public class ProductController {
         return service.getProductById(id);
     }
 
-    @GetMapping("/product/{name}")
-    public Product productByName(String name){
+    @GetMapping("/product")
+    public Product productByName(@RequestParam(value = "name") String name){
         return service.getProductByName(name);
     }
 
--- managementSystem/managementSystem/src/main/java/com/guru/managementSystem/Entity/Product.java
+++ managementSystem/managementSystem/src/main/java/com/guru/managementSystem/Entity/Product.java
@@ -6,9 +6,11 @@ import jakarta.persistence.Id;
 import jakarta.persistence.Table;
 import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Data
 @AllArgsConstructor
+@NoArgsConstructor
 @Entity
 @Table(name = "PRODUCT_TBL")
 public class Product {
